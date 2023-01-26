f1036828c64e5b208707b350b9e2a9b8d88f44de
Emily Power <ekpower@middlebury.edu>
2023-01-26 14:11:31 -0500

    edit weekly assignment 2

---
+++ 2023-01-26-loading-data.R
@@ -0,0 +1,10 @@
+### Lecture 8: Loading in Data
+### 26 January 2023
+### EKP
+
+# Create and save a data set:
+  # write.table(x=varName, file="outputFileName.csv", header=TRUE, sep=",")
+
+# These functions read in a data set:
+  # read.table(file="path/to/data.csv", header=TRUE, sep = ",")
+  # read.csv(file="data.csv", header=TRUE)
